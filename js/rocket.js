d738c98cb41828cf2fac84b57fc5bc8e641912f1
mrra-screencast <mr.richard.armstrong+screencast@gmail.com>
2015-12-12 12:01:59 +0200

    Made my rocket!

---
+++ js/rocket.js
@@ -0,0 +1,35 @@
+var timer = null;
+var countdownNumber = 10;
+
+var changeState = function (state) {
+	document.body.className = 'body-state'+state;
+	clearInterval(timer);
+	countdownNumber = 10;
+	document.getElementById('countdown').innerHTML = countdownNumber;
+
+	// countdown
+	if (state == 2) {
+		timer = setInterval(function () {
+			countdownNumber = countdownNumber-1;
+			document.getElementById('countdown').innerHTML = countdownNumber;
+			
+			if (countdownNumber <= 0) {
+				changeState(3);
+			};
+		}, 500);
+	} else if (state == 3) {
+		var success = setTimeout(function () {
+				var randomNumber = Math.round(Math.random()*10);
+
+				console.log('randomNumber:', randomNumber)
+
+				// succes
+				if (randomNumber > 5) {
+					changeState(4);
+
+				} else {
+					changeState(5); // oh no!
+				}
+		}, 2000);
+	};
+}
