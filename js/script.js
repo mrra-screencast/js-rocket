bf70461867b68765571370dc00123e6dbab9d31a
mrra-screencast <mr.richard.armstrong+screencast@gmail.com>
2015-12-12 11:11:00 +0200

    Added some cool stuff!

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+// this function does this.
+var doCoolStuff = function () {
+	var currentClassName = document.getElementById('cool').className;
+
+	// changing class name
+	if (currentClassName == 'cool') {
+		document.getElementById('cool').className = 'cool red';
+	} else {
+		document.getElementById('cool').className = 'cool';
+	}
+}
+
 var sayMyName = function (name) {
 	alert('My name is: '+name);
 }
